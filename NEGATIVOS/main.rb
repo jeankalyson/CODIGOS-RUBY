84aa235c84f256ca5a56255965d6c2b4203a08c5
Jean Kalyson Pereira de Oliveira <65170046+jeankalyson@users.noreply.github.com>
2022-05-10 16:08:28 -0300

    Update main.rb

--- NEGATIVOS/main.rb
+++ NEGATIVOS/main.rb
@@ -1,10 +1,12 @@
 numero = 0
 num = 0
+quant = 0
 while num>=0
   puts "Digite um número negativo para parar"
   num = gets.chomp.to_i
   if num>0
     numero = numero+num
   end
+  quant = quant+1
 end
-puts "A soma dos números positivos digitados é: #{numero}"
+puts "A média dos números positivos digitados é: #{numero/quant}"
